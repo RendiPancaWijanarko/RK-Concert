07d4b877c3e1b343146ef362b121ffea5dab11ba
kiki <126378515+Riszkys@users.noreply.github.com>
2023-04-01 06:40:47 +0700

    Update scriptLOGINREGISTER.js

--- scriptLOGINREGISTER.js
+++ scriptLOGINREGISTER.js
@@ -4,6 +4,27 @@ const registerLink = document.querySelector(".register-link");
 const btnPopup = document.querySelector(".btnLogin-popup");
 const iconClose = document.querySelector(".icon-close");
 
+const search = () => {
+  const searchbox = document.getElementById("search-item").value.toUpperCase();
+  const storeitem = document.getElementById("konser");
+  const product = document.querySelectorAll(".card");
+  const pname = storeitem.getElementsByTagName("h5");
+
+  for (var i = 0; i < pname.length; i++) {
+    let match = product[i].getElementsByTagName("h5")[0];
+
+    if (match) {
+      let textvalue = match.textContent || match.innerHTML;
+
+      if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+        product[i].style.display = "";
+      } else {
+        product[i].style.display = "none";
+      }
+    }
+  }
+};
+
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
 });
